c8f6e92401be2b42b0f5af01a3a44b8548dd7c9d
entroopie <marcel.boxberger@gmail.com>
2026-02-10 20:26:10 +0100

    added vectorized operations

--- mini_project_1/mandelbrot.py
+++ mini_project_1/mandelbrot.py
@@ -8,7 +8,7 @@ except Exception:
 
 import matplotlib.pyplot as plt
 
-def mandelbrot(xmin, xmax, ymin, ymax, height, width, max_iter):
+def mandelbrot_naive(xmin, xmax, ymin, ymax, height, width, max_iter):
     x = np.linspace(xmin, xmax, width)
     y = np.linspace(ymin, ymax, height)
     mandelbrot_set = np.zeros((height, width))
@@ -19,20 +19,42 @@ def mandelbrot(xmin, xmax, ymin, ymax, height, width, max_iter):
             z = 0
             n = 0
             while n < max_iter and abs(z) <= 2:
-                z = z*z + c
+                z = z ** 2 + c
                 n += 1
             mandelbrot_set[i, j] = n
 
-    image = mandelbrot_set
-    plt.imshow(image, cmap='magma', extent=[xmin, xmax, ymin, ymax])
-    plt.colorbar()
-    plt.title("Mandelbrot Set")
-    plt.show()
+    return mandelbrot_set
 
+def mandelbrot_vectorized(xmin, xmax, ymin, ymax, height, width, max_iter):
+    x = np.linspace(xmin, xmax, width)
+    y = np.linspace(ymin, ymax, height)
+    
+    X, Y = np.meshgrid(x, y)
+    C = X + Y*1j #fancy complex matrix (in python 1j represents imaginary unit)
+    
+    Z = np.zeros(C.shape, dtype=complex)
+    
+    mandelbrot_set = np.zeros(C.shape, dtype=int)
+    mask = np.ones(C.shape, dtype=bool)
+
+    for n in range(max_iter):
+        Z[mask] = Z[mask]**2 + C[mask]
+        escape = mask & (np.abs(Z) > 2)
+        mandelbrot_set[escape] = n
+        mask[escape] = False
+
+    mandelbrot_set[mask] = max_iter #set to max_iter to make not escaped points bright
+    return mandelbrot_set
+    
 
 if __name__ == '__main__':
     max_iter = 100
     xmin, xmax, ymin, ymax = -2.0, 1.0, -1.5, 1.5
     height, width = 1024, 1024  
 
-    mandelbrot(xmin, xmax, ymin, ymax, height, width, max_iter)
+    mandelbrot_set = mandelbrot_vectorized(xmin, xmax, ymin, ymax, height, width, max_iter)
+
+    plt.imshow(mandelbrot_set, cmap='magma', extent=[xmin, xmax, ymin, ymax])
+    plt.colorbar()
+    plt.title("Mandelbrot Set")
+    plt.show()
